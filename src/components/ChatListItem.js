67dca650dda95425782498a29aabff6e2f8c34e0
Patricia Dias Baldez <patriciabaldez1985@gmail.com>
2022-03-29 18:36:10 -0300

    Sidebar ChatList (1/2)

--- src/components/ChatListItem.js
+++ src/components/ChatListItem.js
@@ -3,6 +3,21 @@ import './ChatListItem.css'
 
 export default () => {
     return(
-    <div>...</div>
+    <div className="chatListItem">
+        <img className="chatListItem--avatar" src="https://www.w3schools.com/w3images/avatar2.png" alt="" />
+        <div className="chatListitem--lines">
+            <div className="chatListItem--line">
+                <div className="chatListItem--name">Patricia</div>
+                <div className="chatListItem--date">18:00</div>
+
+            </div>
+            <div className="chatListItem--line">
+                <div className="chatListItem--lastMsg">
+                    <p>Olá, tudo bem?</p>
+                </div>
+
+            </div>
+        </div>
+    </div>
     );
 }
