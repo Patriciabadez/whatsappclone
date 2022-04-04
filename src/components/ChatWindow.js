7ca3cbdf94306084ac7231de8ed47e38971fff9e
Patricia Dias Baldez <patriciabaldez1985@gmail.com>
2022-04-04 13:51:31 -0300

    ContentArea (Chat Window Body)

--- src/components/ChatWindow.js
+++ src/components/ChatWindow.js
@@ -4,34 +4,52 @@ import './ChatWindow.css'
 import SearchIcon from '@mui/icons-material/Search';
 import AttachFileIcon from '@mui/icons-material/AttachFile';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
+import CloseIcon from '@mui/icons-material/Close';
+import SendIcon from '@mui/icons-material/Send';
+import MicIcon from '@mui/icons-material/Mic';
 
 export default () => {
     return (
         <div className="chatWindow">
             <div className="chatWindow--header">
                 <div className="chatWindow--headerinfo">
-                    <img className="chatWindow--avatar" src="https://www.w3schools.com/w3images/avatar2.png" alt=""/>
+                    <img className="chatWindow--avatar" src="https://www.w3schools.com/w3images/avatar2.png" alt="" />
                     <div className="chatWindow--name">Patricia Baldez</div>
                 </div>
                 <div className="chatWindow--headerbuttons">
-                   <div className="chatWindow--btn">
-                       <SearchIcon style={{color: '#919191'}}/>
-                   </div>
-                   <div className="chatWindow--btn">
-                       <AttachFileIcon style={{color: '#919191'}}/>
-                   </div>
-                   <div className="chatWindow--btn">
-                       <MoreVertIcon style={{color: '#919191'}}/>
-                   </div>
+                    <div className="chatWindow--btn">
+                        <SearchIcon style={{ color: '#919191' }} />
+                    </div>
+                    <div className="chatWindow--btn">
+                        <AttachFileIcon style={{ color: '#919191' }} />
+                    </div>
+                    <div className="chatWindow--btn">
+                        <MoreVertIcon style={{ color: '#919191' }} />
+                    </div>
                 </div>
             </div>
             <div className="chatWindow--body">
-
             </div>
             <div className="chatWindow--footer">
-
+                <div className="chatWindow--pre">
+                    <div className="chatWindow--btn">
+                        <EmojiEmotionsIcon style={{ color: '#919191' }} />
+                    </div>
+                </div>
+                <div className="chatWindow--inputarea">
+                    <input 
+                    className="chatWindow--input"
+                    type="text" 
+                    placeholder="Digite uma mensagem"
+                    />
+                </div>
+                <div className="chatWindow--pos">
+                    <div className="chatWindow--btn">
+                        <SendIcon style={{ color: '#919191' }} />
+                    </div>
+                </div>
             </div>
-
         </div>
     )
 
