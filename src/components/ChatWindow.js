8995db3951e135229ee6da6947c7d3e07a107ad4
Patricia Dias Baldez <patriciabaldez1985@gmail.com>
2022-04-07 14:33:52 -0300

    continuação

--- src/components/ChatWindow.js
+++ src/components/ChatWindow.js
@@ -10,20 +10,47 @@ import CloseIcon from '@mui/icons-material/Close';
 import SendIcon from '@mui/icons-material/Send';
 import MicIcon from '@mui/icons-material/Mic';
 
-
 export default () => {
+
+    let recognition = null;
+    let SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+    if(SpeechRecognition !== undefined) {
+        recognition = new SpeechRecognition();
+
+    }
+
     const [emojiOpen, setEmojiOpen] = useState(false);
     const [text, setText] = useState('');
+    const [listening, setListening] = useState(false);
 
     const handleEmojiClick = (e, emojiObject) => {
         // console.log(emojiObject);
-        setText( text + emojiObject.emoji );
+        setText(text + emojiObject.emoji);
 
     }
     const handleOpenEmoji = () => {
         setEmojiOpen(true);
     }
     const handleCloseEmoji = () => {
+        setEmojiOpen(false);
+    }
+    const handleMicClick = () => {
+       if(recognition !== null) {
+           
+        recognition.onstart = () => {
+            setListening(true);
+        }
+        recognition.onend = () => {
+            setListening(false);
+        }
+        recognition.onresult = (e) => {
+            setText (e.results[0][0].transcript);
+        }
+        recognition.start();
+
+       }
+    }
+    const handleSendClick = () => {
         setEmojiOpen(false);
     }
 
@@ -65,15 +92,15 @@ export default () => {
                     <div
                         className="chatWindow--btn"
                         onClick={handleCloseEmoji}
-                        style={{width: emojiOpen?40:0}}
-                        >
+                        style={{ width: emojiOpen ? 40 : 0 }}
+                    >
                         <CloseIcon style={{ color: '#919191' }} />
                     </div>
                     <div
                         className="chatWindow--btn"
                         onClick={handleOpenEmoji}
                     >
-                        <InsertEmoticonIcon style={{ color: emojiOpen?'#009688': '#919191' }} />
+                        <InsertEmoticonIcon style={{ color: emojiOpen ? '#009688' : '#919191' }} />
                     </div>
                 </div>
                 <div className="chatWindow--inputarea">
@@ -82,13 +109,20 @@ export default () => {
                         type="text"
                         placeholder="Digite uma mensagem"
                         value={text}
-                        onChange={e=>setText(e.target.value)}
+                        onChange={e => setText(e.target.value)}
                     />
                 </div>
                 <div className="chatWindow--pos">
-                    <div className="chatWindow--btn">
-                        <SendIcon style={{ color: '#919191' }} />
-                    </div>
+                    {text === '' &&
+                        <div onClick={handleMicClick}className="chatWindow--btn">
+                            <MicIcon style={{ color: listening ? '#126ECE' :'#919191' }} />
+                        </div>
+                    }
+                    {text !== '' &&
+                        <div onClick={handleSendClick} className="chatWindow--btn">
+                            <SendIcon style={{ color: '#919191' }} />
+                        </div>
+                    }
                 </div>
             </div>
         </div>
